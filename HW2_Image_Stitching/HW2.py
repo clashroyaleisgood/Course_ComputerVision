2ac70b3065f981d10ab43f01c0d4eeb4d2863cfc
clashroyaleisgood <oscar19971003@gmail.com>
2022-05-01 18:57:32 +0800

    finish RANSAC, edit RANSAC prototype( remove f1, f2, add thres, repeat )

--- HW2_Image_Stitching/HW2.py
+++ HW2_Image_Stitching/HW2.py
@@ -77,7 +77,7 @@ def kNN(featureImage1, featureImage2, threshold=1.33):
                distance < thresold_dist2:
                 find_close_dist2 = True
                 break
-                
+
         # for idx2, feat2 in enumerate(featureImage2):
         #     distance = dist(feat1, feat2)
         #     if distance < min_dist:
@@ -122,39 +122,56 @@ def get_HomographyMatrix(matches4):
     H = H / H[8]  # normalize A33 to 1
     H = H.reshape((3, 3))
     return H
-    
 
-def RANSAC(matches, kp1, f1, kp2, f2):
+
+def RANSAC(matches, kp1, kp2, threshold=3, repeat=5):
     '''
     random choose 4 from matches
     do PrespectiveTransform(all matches)
         and counting supports
     return best Transform Mat
     '''
-    rand4number = np.random.choice(len(matches), 4)
-    match4 = []
-    for i in range(4):
-        idx = rand4number[i]
-        m = [
-            kp1[matches[idx][0]].pt,
-            kp2[matches[idx][1]].pt
-        ]  # [[x1, y1], [x1h, y1h]]
-        match4 += [m]
+    max_support = 0
+    max_H = None
+    
+    for _ in range(repeat):
+        rand4number = np.random.choice(len(matches), 4)
+        match4 = []
+        for i in range(4):
+            idx = rand4number[i]
+            m = [
+                kp1[matches[idx][0]].pt,
+                kp2[matches[idx][1]].pt
+            ]  # [[x1, y1], [x1h, y1h]]
+            match4 += [m]
 
-    H = get_HomographyMatrix(match4)
+        H = get_HomographyMatrix(match4)
 
-    # correctness test
-    # p1 = np.array([e for e in kp1[matches[rand4number[3]][0]].pt] + [1]).reshape((3, 1))
-    # p2 = np.array([e for e in kp2[matches[rand4number[3]][1]].pt] + [1]).reshape((3, 1))
-    # print(p1)
-    # print(p2)
-    # p3 = H @ p1
-    # p3 /= p3[2]
-    # print(p3)
+        # correctness test
+        # p1 = np.array([e for e in kp1[matches[rand4number[3]][0]].pt] + [1]).reshape((3, 1))
+        # p2 = np.array([e for e in kp2[matches[rand4number[3]][1]].pt] + [1]).reshape((3, 1))
+        # print(p1)
+        # print(p2)
+        # p3 = H @ p1
+        # p3 /= p3[2]
+        # print(p3)
 
-    # for idx1, idx2 in matches:
-    #     # H * kp1[idx1].pt -> kp2[idx2].pt
-    #     pass
+        support = 0
+        for idx1, idx2 in matches:
+            p1 = np.array([*(kp1[idx1].pt), 1])  # [x, y, 1]
+            p2_proj = H @ p1
+            p2_proj = p2_proj[:2] / p2_proj[2]  # [wx, wy, w] -> [x, y]
+
+            p2 = np.array(kp2[idx2].pt)
+
+            distance = np.linalg.norm(p2 - p2_proj)
+
+            if distance < threshold:
+                support += 1
+        # end calc suport
+        if support > max_support:
+            max_H = H
+    return max_H
 
 
 def combine(image1, image2):
@@ -178,7 +195,8 @@ def combine(image1, image2):
     # create_im_window('im2', image2)
     # im_show()
 
-    RANSAC(matches, kp1=kp1, f1=f1, kp2=kp2, f2=f2)
+    H = RANSAC(matches, kp1=kp1, kp2=kp2)
+
 
     # bestMat = RANSAC(matches, kp1=kp1, f1=f1, kp2=kp2, f2=f2)
 
