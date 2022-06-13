3232e745db9ca7adf9ca6299d6bbd93c2a4bed1f
clashroyaleisgood <oscar19971003@gmail.com>
2022-06-13 13:10:21 +0800

    overall architecture

---
+++ Final_Project/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+from datasets import getDataset
+from disparity import getDisparityMap, getDepthMap, visualizeDepthMap
+# form rectification import rectify_method, ...
+
+if __name__ == '__main__':
+    # implementation of all
+    pass
---
+++ Final_Project/rectification.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2
+
+from datasets import getDataset
+
+
+
+if __name__ == '__main__':
+    pass
