7314144eb9925c16782412cb6eed90adaebf7c37
clashroyaleisgood <oscar19971003@gmail.com>
2022-06-13 13:37:25 +0800

    rectification prototype

--- Final_Project/rectification.py
+++ Final_Project/rectification.py
@@ -3,7 +3,19 @@ import cv2
 
 from datasets import getDataset
 
+def rectify(image_l, image_r):
+    '''
+    input un-rectified images (tilted images)
+    return rectified images
 
+    steps:
+    - find cooresponding point [pairs]
+    - find Fundamental [matrix] by [pairs]
+    - find H1, H2: Homography matrices to wrap images
+      from [pairs] and [matrix]
+    - wrapPerspective(image_l, H1), ...
+    '''
+    pass
 
 if __name__ == '__main__':
     pass
